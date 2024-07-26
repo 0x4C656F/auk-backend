b88e00d3a1c845fd28906c34e89467b63533f60b
Lev Lavryniuk <levlavryniuk@outlook.com>
2024-07-26 23:50:08 +0200

    chore: Remove unused code in main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  });
+
   app.use(cookieParser());
 
   await app.listen(process.env.PORT || 10000);
